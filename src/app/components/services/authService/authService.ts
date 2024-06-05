1b577d087a4a1145e560481ec6c48e6e201a2e54
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-06-05 16:26:06 +0100

    Updating git on the progress so far

---
+++ src/app/components/services/authService/authService.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+// import { baseURL } from "@/app/constants";
+import { baseURL } from "../../../constants";
+
+export const createGuestSession = async () => {
+  const res = await axios.get(`${baseURL}authentication/guest_session/new`, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YmZlZWFhNDQ3M2UyYjdkZTBiNmQwY2NmODIzNTc4OCIsInN1YiI6IjY2MTQ2MjBlNTQzN2Y1MDE4NDdlNzFmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QPmWP9Cnp2YcpYjwmpnN1t_aMWvlna9Y-QKl_7LC87I",
+    },
+  });
+
+  console.log(process.env.NEXT_PUBLIC_API_KEY);
+  return res.data;
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
 "use client";
 import Dashboard from "./components/dashboard/dashboard";
 import "semantic-ui-css/semantic.min.css";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Home() {
   const queryClient = new QueryClient();
