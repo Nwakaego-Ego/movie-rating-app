e9d07280a2eda97fa122d42d9db5eca228aac8ca
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-06-14 23:15:20 +0100

    i added the popular movie API

--- src/app/components/dashboard/dashboard.jsx
+++ src/app/components/dashboard/dashboard.jsx
@@ -1,116 +1,6 @@
-// import React, { useEffect } from "react";
-// import Navbar from "../navbar/navbar";
-// import { useQuery, useMutation } from "@tanstack/react-query";
-// import { createGuestSession } from "../services/authService/authService";
-
-// export default function () {
-//   const { data, isLoading, error, refetch } = useQuery({
-//     queryKey: ["movieData"],
-//     queryFn: () => createGuestSession(),
-//     enabled: false,
-//     onSuccess: (data) => {
-//       console.log(data);
-//       localStorage.setItem(`guest`, data?.data?.guest_session_id);
-//     },
-//   });
-
-//   console.log(data);
-
-//   const guestSession = () => {
-//     refetch();
-//     //  sessionStorage.setItem(`guest`, data?.guest_session_id);
-//   };
-
-//   useEffect(() => {
-//     if (data && data?.data?.guest_session_id) {
-//       sessionStorage.setItem("guest", data?.data?.guest_session_id);
-//     }
-//     console.log("useEffect hook is triggered");
-//   }, [data]);
-
-//   console.log(sessionStorage.getItem("guest"));
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="home flex justify-center">Home</div>
-//       <div className="auth-main">
-//         <div className="greeting">
-//           Welcome! Login by registering as a{" "}
-//           <span className="guest">Guest below</span>
-//         </div>
-//         <div>
-//           <button className="auth-login">
-//             {isLoading ? "Logging in..." : "Login"}
-//           </button>
-//           <div>
-//             {/* <button onClick={() => refetch()}>Login as Guest</button> */}
-//             <button onClick={guestSession} disabled={isLoading}>
-//               {isLoading ? "1...2...3" : "Login as Guest"}
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// import React, { useEffect } from "react";
-// import Navbar from "../navbar/navbar";
-// import { useQuery, useMutation } from "@tanstack/react-query";
-// import { createGuestSession } from "../services/authService/authService";
-
-// export default function dashboard() {
-//   const { data, isLoading, error, refetch } = useQuery({
-//     queryKey: ["movieData"],
-//     queryFn: () => createGuestSession(),
-//     enabled: false,
-//   });
-
-//   useEffect(() => {
-//     if (data && data?.data?.guest_session_id) {
-//       sessionStorage.setItem("guest", data?.data?.guest_session_id);
-//     }
-//     console.log(data);
-//     console.log("useEffect hook is triggered");
-//   }, [isLoading]);
-
-//   const guestSession = async () => {
-//     const fetchedData = await createGuestSession();
-//     if (fetchedData && fetchedData.data && fetchedData.data.guest_session_id) {
-//       sessionStorage.setItem("guest", fetchedData.data.guest_session_id);
-//     }
-//     refetch();
-//   };
-
-//   console.log(sessionStorage.getItem("guest"));
-
-//   return (
-//     <>
-//       <Navbar />
-//       <div className="home flex justify-center">Home</div>
-//       <div className="auth-main">
-//         <div className="greeting">
-//           Welcome! Login by registering as a{" "}
-//           <span className="guest">Guest below</span>
-//         </div>
-//         <div>
-//           <button className="auth-login">
-//             {isLoading ? "Logging in..." : "Login"}
-//           </button>
-//           <div>
-//             <button onClick={guestSession} disabled={isLoading}>
-//               {isLoading ? "1...2...3" : "Login as Guest"}
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
 "use client";
 import React, { useEffect } from "react";
+import "./dashboard.css";
 import Navbar from "../navbar/navbar";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { createGuestSession } from "../services/authService/authService";
@@ -164,9 +54,16 @@ export default function Dashboard() {
             {isLoading ? "Logging in..." : "Login"}
           </button>
           <div>
-            <button onClick={guestSession} disabled={isLoading}>
-              {isLoading ? "1...2...3" : "Login as Guest"}
+            <button
+              onClick={guestSession}
+              disabled={isLoading}
+              style={{ backgroundColor: isLoading ? "blue" : "yellow" }}
+            >
+              {isLoading ? "1...2...3 " : "Login as Guest "}
             </button>
+          </div>
+          <div>
+            <button className="styled-button">Login</button>
           </div>
         </div>
       </div>
--- src/app/components/login/page.jsx
+++ src/app/components/login/page.jsx
@@ -10,15 +10,9 @@ export default function page() {
           <span className="guest">Guest below</span>
         </div>
         <div>
-          <button className="auth-login" onClick={login}>
-            Login
-          </button>
+          <button className="auth-login">Login</button>
         </div>
-        <div>
-          <button className="style-button" onClick={login}>
-            Login
-          </button>
-        </div>
+        <div></div>
       </div>
     </>
   );
